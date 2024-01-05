9b8d56ccc9b5715f7d9f79492cd84d033307b05d
rohan panchal <rohanpanchal216@gmail.com>
2024-01-05 12:20:43 +0530

    work on it

---
+++ assets/js/app.js
@@ -0,0 +1,38 @@
+$('#testimonialsOwlCarousel2').owlCarousel({
+    loop: true,
+    margin:10,
+    dots: false,
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$('#testimonialsOwlCarousel').owlCarousel({
+    loop: true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
